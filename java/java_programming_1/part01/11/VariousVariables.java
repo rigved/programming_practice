21484c1774e07459360292f26f25f366d5aaa5a1
Rigved Rakshit <r.phate@gmail.com>
2020-07-05 16:49:18 +0530

    Java various variable assignments

---
+++ java/java_programming_1/part01/11/VariousVariables.java
@@ -0,0 +1,40 @@
+/*
+ * VariousVariables.java
+ * Copyright (C) 2020  Rigved Rakshit
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+
+public class VariousVariables
+{
+    public static void main(String[] args)
+    {
+        int numberOfChicken = 9000;
+        double baconWeight = 0.1;
+        String tractor = "Zetor";
+
+        System.out.println("Chicken:");
+        System.out.println(numberOfChicken);
+        System.out.println("Bacon (kg):");
+        System.out.println(baconWeight);
+        System.out.println("Tractor:");
+        System.out.println(tractor);
+        System.out.println("");
+        System.out.println("And finally, a summary:");
+        System.out.println(numberOfChicken);
+        System.out.println(baconWeight);
+        System.out.println(tractor);
+    }
+}
