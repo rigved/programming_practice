ac3b6e4031206ace60bfa8a12aafea88c13929f9
Rigved Rakshit <r.phate@gmail.com>
2020-07-06 21:42:53 +0530

    Java another if-else condition

---
+++ java/java_programming_1/part01/29/Adulthood.java
@@ -0,0 +1,42 @@
+/*
+ * Adulthood.java
+ * Copyright (C) 2020  Rigved Rakshit
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+
+import java.util.Scanner;
+
+public class Adulthood
+{
+    public static void main(String[] args)
+    {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("How old are you?");
+        int age = Integer.valueOf(scan.nextLine());
+
+        if (age >= 18)
+        {
+            System.out.println("You are an adult");
+        }
+        else
+        {
+            System.out.println("You are not an adult");
+        }
+
+        scan.close();
+    }
+}
