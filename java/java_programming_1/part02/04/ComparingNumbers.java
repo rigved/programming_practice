c956273821d7edc7e37d5974e01ba9d5c5392752
Rigved Rakshit <r.phate@gmail.com>
2020-07-12 18:37:54 +0530

    Java Largest number

---
+++ java/java_programming_1/part02/04/ComparingNumbers.java
@@ -0,0 +1,46 @@
+/*
+ * ComparingNumbers.java
+ * Copyright (C) 2020  Rigved Rakshit
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+
+import java.util.Scanner;
+
+public class ComparingNumbers
+{
+    public static void main(String[] args)
+    {
+        Scanner scanner = new Scanner(System.in);
+
+        int firstNumber = Integer.valueOf(scanner.nextLine());
+        int secondNumber = Integer.valueOf(scanner.nextLine());
+
+        if (firstNumber > secondNumber)
+        {
+            System.out.println(firstNumber + " is greater than " + secondNumber + ".");
+        }
+        else if (firstNumber < secondNumber)
+        {
+            System.out.println(firstNumber + " is smaller than " + secondNumber + ".");
+        }
+        else
+        {
+            System.out.println(firstNumber + " is equal to " + secondNumber + ".");
+        }
+
+        scanner.close();
+    }
+}
