76c2920e8b136938edb693b7ecc508ff0a1f263c
Rigved Rakshit <r.phate@gmail.com>
2020-07-18 17:46:42 +0530

    Java Find the sum of a sequence of numbers from 1 to n

---
+++ java/java_programming_1/part02/17/SumOfASequence.java
@@ -0,0 +1,40 @@
+/*
+ * SumOfASequence.java
+ * Copyright (C) 2020  Rigved Rakshit
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+
+import java.util.Scanner;
+
+public class SumOfASequence
+{
+    public static void main(String[] args)
+    {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Last number? ");
+        int lastNumber = Integer.valueOf(scanner.nextLine()), sum = 0;
+
+        for (int i = 1; i <= lastNumber; i++)
+        {
+            sum += i;
+        }
+
+        System.out.println("The sum is " + sum);
+
+        scanner.close();
+    }
+}
