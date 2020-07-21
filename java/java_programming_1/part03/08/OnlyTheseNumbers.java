5c3c10fd9e7f59533287b111df8b921e3bddf0dc
Rigved Rakshit <r.phate@gmail.com>
2020-07-21 12:50:02 +0530

    Java ArrayList print a slice of the list

---
+++ java/java_programming_1/part03/08/OnlyTheseNumbers.java
@@ -0,0 +1,54 @@
+/*
+ * OnlyTheseNumbers.java
+ * Copyright (C) 2020  Rigved Rakshit
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class OnlyTheseNumbers
+{
+    public static void main(String[] args)
+    {
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<Integer> numbers = new ArrayList<>();
+
+        while (true)
+        {
+            int number = Integer.valueOf(scanner.nextLine());
+
+            if (number == -1)
+            {
+                break;
+            }
+
+            numbers.add(number);
+        }
+
+        System.out.print("From where? ");
+        int startIndex = Integer.valueOf(scanner.nextLine());
+        System.out.print("To where? ");
+        int endIndex = Integer.valueOf(scanner.nextLine());
+
+        for (int i = startIndex; i <= endIndex; i++)
+        {
+            System.out.println(numbers.get(i));
+        }
+
+        scanner.close();
+    }
+}
