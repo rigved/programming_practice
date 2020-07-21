6f40486aa86138fd6fd4e02cf6d4264c245c6d7a
Rigved Rakshit <r.phate@gmail.com>
2020-07-21 12:13:22 +0530

    Java ArrayList size

---
+++ java/java_programming_1/part03/04/ListSize.java
@@ -0,0 +1,45 @@
+/*
+ * ListSize.java
+ * Copyright (C) 2020  Rigved Rakshit
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class ListSize
+{
+    public static void main(String[] args)
+    {
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<String> list = new ArrayList<>();
+
+        while (true)
+        {
+            String input = scanner.nextLine();
+            if (input.equals(""))
+            {
+                break;
+            }
+
+            list.add(input);
+        }
+
+        System.out.println("In total: " + list.size());
+
+        scanner.close();
+    }
+}
