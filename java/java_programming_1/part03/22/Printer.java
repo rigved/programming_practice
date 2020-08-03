cf7e2adf5c1398a5409c23e8bbf38bd9592d40e3
Rigved Rakshit <r.phate@gmail.com>
2020-08-03 18:59:11 +0530

    Java Print asterisks based on values in an array passed as a parameter to a function

---
+++ java/java_programming_1/part03/22/Printer.java
@@ -0,0 +1,40 @@
+/*
+ * Printer.java
+ * Copyright (C) 2020  Rigved Rakshit
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+
+public class Printer
+{
+    public static void main(String[] args)
+    {
+        int[] array = {5, 1, 3, 4, 2};
+        printArrayInStars(array);
+    }
+
+    public static void printArrayInStars(int[] array)
+    {
+        for (int i = 0; i < array.length; i++)
+        {
+            for (int j = 0; j < array[i]; j++)
+            {
+                System.out.print("*");
+            }
+
+            System.out.println("");
+        }
+    }
+}
