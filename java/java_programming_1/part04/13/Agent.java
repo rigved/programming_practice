3906277c02523639760d2d030b518976eb6cf706
Rigved Rakshit <r.phate@gmail.com>
2020-08-05 11:33:04 +0530

    Java Return a String representation of a class instance

---
+++ java/java_programming_1/part04/13/Agent.java
@@ -0,0 +1,35 @@
+/*
+ * Agent.java
+ * Copyright (C) 2020  Rigved Rakshit
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+
+public class Agent
+{
+    private String firstName;
+    private String lastName;
+
+    public Agent(String initFirstName, String initLastName)
+    {
+        this.firstName = initFirstName;
+        this.lastName = initLastName;
+    }
+
+    public String toString()
+    {
+        return ("My name is " + this.lastName + ", " + this.firstName + " " + this.lastName);
+    }
+}
---
+++ java/java_programming_1/part04/13/Main.java
@@ -0,0 +1,13 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Agent bond = new Agent("James", "Bond");
+
+        bond.toString();
+        System.out.println(bond);
+
+        Agent iconic = new Agent("Iconic", "Bond");
+        System.out.println(iconic);
+    }
+}
