76ace2eb7bde22d281468b1e395a8bf1f4864abd
Rigved Rakshit <r.phate@gmail.com>
2020-08-07 16:30:54 +0530

    Java Modify objects passed as parameters to methods

---
+++ java/java_programming_1/part05/09/HealthStation.java
@@ -0,0 +1,44 @@
+/*
+ * HealthStation.java
+ * Copyright (C) 2020  Rigved Rakshit
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+
+public class HealthStation
+{
+    private int weighingsCount;
+
+    public HealthStation()
+    {
+        this.weighingsCount = 0;
+    }
+
+    public int weighings()
+    {
+        return weighingsCount;
+    }
+
+    public int weigh(Person person)
+    {
+        weighingsCount++;
+        return person.getWeight();
+    }
+
+    public void feed(Person person)
+    {
+        person.setWeight(person.getWeight() + 1);
+    }
+}
