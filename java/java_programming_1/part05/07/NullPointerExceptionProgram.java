15c91a028e9a71d800600c10e04a3eacf957073f
Rigved Rakshit <r.phate@gmail.com>
2020-08-07 13:21:51 +0530

    Java Null Pointer Exception

---
+++ java/java_programming_1/part05/07/NullPointerExceptionProgram.java
@@ -0,0 +1,41 @@
+/*
+ * NullPointerExceptionProgram.java
+ * Copyright (C) 2020  Rigved Rakshit
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+
+import java.util.ArrayList;
+
+public class NullPointerExceptionProgram
+{
+    public static void main(String[] args)
+    {
+        ArrayList<String> lines = new ArrayList<>();
+        lines.add("Never has a man influenced physics so profoundly as Niels Bohr in the early 1900's");
+        lines.add("Going back to this time period, little was known about atomic structure; Bohr set out");
+        lines.add("to end the obscurity of physics. However, things didn't come easy for Bohr. He had to");
+        lines.add("give up most of his life for physics and research of many hypothesis. But, this is why");
+        lines.add("you and I have even heard of the quantum theory and atomic structures. Bohr came");
+        lines.add("up with his quantum theory while studying...");
+
+        lines = null;
+
+        for (String line: lines)
+        {
+            System.out.println(line);
+        }
+    }
+}
